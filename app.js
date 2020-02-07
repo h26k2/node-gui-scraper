3ecc38a17119137ee1e8a6281a55e7f3f96aa106
h26k2 <hasnain.karim262@outlook.com>
2020-02-07 19:01:11 +0500

    completed individual product so that we can start scraping of the individual product page

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.set("view engine","ejs");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : true}));
 
-const openPuppeteer = async(url  , filename , brandName , uniqueCheck , isLinkElement) =>{
+const selectMainContainer = async(url  , filename , brandName , uniqueCheck ) =>{
 
     let browser = await puppeteer.launch({headless: false});
 
@@ -20,7 +20,7 @@ const openPuppeteer = async(url  , filename , brandName , uniqueCheck , isLinkEl
     
     await page.addStyleTag({content : '.h26k2-color{background : yellow!important}'});
     
-    await page.evaluate((filename , brandName , uniqueCheck , isLinkElement)=>{
+    await page.evaluate((filename , brandName , uniqueCheck )=>{
         
         let prev = undefined;
         let data = [];
@@ -48,43 +48,14 @@ const openPuppeteer = async(url  , filename , brandName , uniqueCheck , isLinkEl
             
             let elem = e.target;
             
-            let anchors = elem.getElementsByTagName("a");
-
-            for(let i=0 ; i<anchors.length ; i++){
-                
-                let a_href = anchors[i].getAttribute("href");
-                let a_class = anchors[i].getAttribute("class");
-
-                let confirm_string = `URL : ${a_href}\nElement : ${a_class}\nDo you want to want to select this element ? `;
-
-                if(confirm(confirm_string)){
-                    alert("done");
-                    break;
-                }
-                else{
-                    continue;
-                }
-
-
-            }
-
-           
-
-            return;
             if(elem !== undefined && elem.getAttribute("data-name") == undefined){
 
                 let status = window.confirm("Select this element ? ");
                 
                 if(status == true){
-                    
-
-                    if(isLinkElement){
-                        console.log(elem);
-                    }
-                    else{
-                        elem = elem.getAttribute("class");
-                        elem = elem.replace("h26k2-color","");
-                    }
+                   
+                    elem = elem.getAttribute("class");
+                    elem = elem.replace("h26k2-color","")
 
                     let dom_elem = document.getElementsByClassName(elem);
                     
@@ -92,7 +63,7 @@ const openPuppeteer = async(url  , filename , brandName , uniqueCheck , isLinkEl
                         
                         data = elem;
                         console.log(data);
-                        alert(data); /*
+                        alert(data); 
                         try{
                             const blob = new Blob([`${filename}\n${data}`],{type : 'text/csv'});
                             const url = window.URL.createObjectURL(blob);
@@ -111,7 +82,7 @@ const openPuppeteer = async(url  , filename , brandName , uniqueCheck , isLinkEl
                         catch(err){
                             alert(`error occured while downloading meta file\n Check console for error`);
                             console.log(err);
-                        }*/
+                        }
 
                     }
                     else{
@@ -125,10 +96,110 @@ const openPuppeteer = async(url  , filename , brandName , uniqueCheck , isLinkEl
 
         });
 
-    },filename  , brandName , uniqueCheck , isLinkElement);
+    },filename  , brandName , uniqueCheck );
 
 
 }
+
+const selectProduct = async (url,filename,brandName , uniqueCheck) => {
+
+    let browser = await puppeteer.launch({headless: false});
+
+    let page = await browser.newPage();
+
+    await page.goto(url,{waitUntil : 'networkidle2' , timeout : 0 });
+    
+    await page.addStyleTag({content : '.h26k2-color{background : yellow!important}'});
+
+    await page.evaluate((filename,brandName,uniqueCheck)=>{
+        
+        let prev = undefined;
+        let data = [];
+        
+        window.addEventListener("mouseover",(e)=>{
+
+            let current = e.target;
+            
+            if(current !== undefined && current.classList.contains("h26k2-color") != true){
+                current.classList.add("h26k2-color");
+                prev = current;
+            }
+            
+        });
+
+        window.addEventListener("mouseout",(e)=>{
+    
+            if(prev !== undefined && prev.classList.contains("h26k2-color") == true){
+                prev.classList.remove("h26k2-color");
+            }
+        
+        });
+
+        window.addEventListener("click",async(e)=>{
+            
+            let elem = e.target;
+            
+            if(elem != undefined && elem.getAttribute("data-name") == undefined){
+
+                let anchors = elem.getElementsByTagName("a");
+                let element_classes = elem.getAttribute("class");
+
+                for(let i=0 ; i<anchors.length ; i++){
+                
+                    let a_href = anchors[i].getAttribute("href");
+                    let a_class = anchors[i].getAttribute("class");
+    
+                    let confirm_string = `URL : ${a_href}\nElement : ${a_class}\nDo you want to want to select this element ? `;
+    
+                    if(confirm(confirm_string)){
+
+                        let product_string = `Please enter the name you want us to target on individual product from the given below data\n${element_classes}`;
+
+                        let elem = prompt(product_string);
+                        
+                        alert("done");
+
+                        let data = `${a_class}|${elem}`;
+
+                        try{
+                            const blob = new Blob([`${filename}\n${data}`],{type : 'text/csv'});
+                            const url = window.URL.createObjectURL(blob);
+                            const a = document.createElement('a');
+                            a.setAttribute("data-name","csv");
+                            a.setAttribute('hidden','');
+                            a.setAttribute('href',url);
+                            a.setAttribute('download',`${brandName}_${filename}.csv`);
+                            document.body.appendChild(a);
+                            a.click();
+                            document.body.removeChild(a);
+
+                            alert(`successfully saved metafile as : ${brandName}_${filename}.csv\n You can now close this window`);
+                            
+                        }
+                        catch(err){
+                            alert(`error occured while downloading meta file\n Check console for error`);
+                            console.log(err);
+                        }
+
+                        break;
+                    }
+                    else{
+                        continue;
+                    }
+    
+    
+                }
+    
+            }
+            
+        });
+
+
+    },filename,brandName,uniqueCheck);
+
+
+}
+
 
 
 app.get("/",async(req,res) => {
@@ -139,10 +210,7 @@ app.post("/mainProducts",async(req,res)=>{
 
     let {url , filename ,brand} = req.body;
 
-    let uniqueCheck = true;
-    let isLinkElement = false;
-
-    openPuppeteer(url, filename,brand , uniqueCheck , isLinkElement);
+    selectMainContainer(url, filename,brand ,true);
 
 }); 
 
@@ -151,10 +219,7 @@ app.post("/productPageLink",async(req,res)=>{
 
     let {url , filename ,brand} = req.body;
 
-    let uniqueCheck = false;
-    let isLinkElement = true;
-
-    openPuppeteer(url,filename,brand , uniqueCheck , isLinkElement);
+    selectProduct(url,filename,brand , false );
 
 });
 
