be4d056734380b548483f2cef217584881b33a3b
h26k2 <hasnain.karim262@outlook.com>
2020-02-26 16:19:00 +0500

    individual product completed

--- controller/requests/individualProduct.js
+++ controller/requests/individualProduct.js
@@ -59,23 +59,84 @@ const individualProduct = (app,puppeteer) => {
                 if(elem != undefined && elem.getAttribute("data-name") == undefined){
     
                     let anchors = elem.getElementsByTagName("a");
-                    let element_classes = elem.getAttribute("class");
+                    let element_classes = elem.parentElement.getAttribute("class");
+                    let  clicked_elem_classes = elem.getAttribute("class");
+                    let dataToSend ;
     
                     for(let i=0 ; i<anchors.length ; i++){
                     
                         let a_href = anchors[i].getAttribute("href");
                         let a_class = anchors[i].getAttribute("class");
         
-                        let confirm_string = `URL : ${a_href}\nElement : ${a_class}\nDo you want to want to select this element ? `;
-        
-                        if(confirm(confirm_string)){
-    
-                            let product_string = `Please enter the name you want us to target on individual product from the given below data\n${element_classes}`;
-    
-                            let elem = prompt(product_string);
-                            alert("done");
-                            let data = `${a_class}|${elem}`;
-                            console.log(`h26k2-data:${data}`);
+                        let confirm_string = `URL : ${a_href}\nElement : ${a_class}\nIs this the product URL ?  `;
+                        
+                        
+                        if(confirm(confirm_string) ){
+                           
+                            if(elem.getElementsByClassName(a_class).length == 1){
+                                dataToSend = `class|${a_class}`;
+                            }
+                            else{
+                                
+                                let temp_an = anchors[i];
+                                let structureDetails = [];
+
+                                //for getting XPATH of the element
+
+                                while(true){
+                                    
+                                    let len = structureDetails.length ; 
+                                    let temp_an_cls = temp_an.parentElement.getAttribute("class");
+
+                                    if( temp_an_cls == element_classes ){
+
+                                        if(len < 1){
+                                            structureDetails.push(
+                                                {
+                                                    elem : temp_an.parentElement.nodeName,
+                                                    index : [...temp_an.parentElement.children].indexOf(temp_an)
+                                                }
+                                            );
+                                        }
+                                        break;
+                                    }
+                                    
+                                    structureDetails.push(
+                                        {
+                                            elem : temp_an.parentElement.nodeName,
+                                            index : [...temp_an.parentElement.children].indexOf(temp_an)
+                                        }
+                                    );
+
+                                    temp_an = temp_an.parentElement;
+
+                                }
+
+                                //For printing the XPATH structure of the string
+
+                                let structureString = ``;
+
+                                for(let i=structureDetails.length  - 1; i>= 0 ; i--){
+                        
+                                    let s_elem = structureDetails[i].elem;
+                                    let index = structureDetails[i].index;
+            
+                                    structureString += `${s_elem}[${index}]/`
+                                }
+            
+                                structureString = structureString.substr(0,structureString.length - 1);
+                                alert(`Done! Xpath is : ${structureString}`);
+                                dataToSend = `xpath|${structureString}`;
+
+                            }
+
+                            
+                            let product_string = `Please enter the name you want us to target on individual product from the given below data\n${clicked_elem_classes}`;
+                            
+                            let product_elem = prompt(product_string);
+                            alert(`Successfully Selected!\nYou can close the window.`);
+                            
+                            console.log(`h26k2-data:[${product_elem}]${dataToSend}`);
     
                             break;
                         }
