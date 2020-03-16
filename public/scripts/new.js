de0ce91d2a1279c1c4f01ddd50691b8a81e626a6
h26k2 <hasnain.karim262@outlook.com>
2020-03-16 15:35:38 +0500

    some updates

--- public/scripts/new.js
+++ public/scripts/new.js
@@ -12,13 +12,8 @@ let demo = [
     
     Receive 15% off when you pay through Moneygram, Western Union, or wire transfer.`,
 
-    ["https://officialchansneakers.com/image/cache/catalog/d_social_login/48-570x570.jpeg",
-    "https://officialchansneakers.com/image/cache/catalog/d_social_login/44-570x570.jpeg",
-    "https://officialchansneakers.com/image/cache/catalog/Adidas/16-570x570.JPG",
-    "https://officialchansneakers.com/image/cache/catalog/d_social_login/27-570x570.jpeg]"]
-
     ],
-    ["Bottega Vaenta Clutch 2","$289.00","Categories Bags Bottega Veneta" ,`Description
+    ["Bottega Vaenta Clutch 1","$289.00","Categories Bags Bottega Veneta" ,`Description
 
     Product code #191213-23
     100% Genuine Leather Matching Quality of Original Louis Vuitton Production (imported from Europe)
@@ -30,13 +25,8 @@ let demo = [
     
     Receive 15% off when you pay through Moneygram, Western Union, or wire transfer.`,
 
-    ["https://officialchansneakers.com/image/cache/catalog/d_social_login/48-570x570.jpeg",
-    "https://officialchansneakers.com/image/cache/catalog/d_social_login/44-570x570.jpeg",
-    "https://officialchansneakers.com/image/cache/catalog/Adidas/16-570x570.JPG",
-    "https://officialchansneakers.com/image/cache/catalog/d_social_login/27-570x570.jpeg]"]
-
     ],
-    ["Bottega Vaenta Clutch 3","$289.00","Categories Bags Bottega Veneta" ,`Description
+    ["Bottega Vaenta Clutch 1","$289.00","Categories Bags Bottega Veneta" ,`Description
 
     Product code #191213-23
     100% Genuine Leather Matching Quality of Original Louis Vuitton Production (imported from Europe)
@@ -48,13 +38,8 @@ let demo = [
     
     Receive 15% off when you pay through Moneygram, Western Union, or wire transfer.`,
 
-    ["https://officialchansneakers.com/image/cache/catalog/d_social_login/48-570x570.jpeg",
-    "https://officialchansneakers.com/image/cache/catalog/d_social_login/44-570x570.jpeg",
-    "https://officialchansneakers.com/image/cache/catalog/Adidas/16-570x570.JPG",
-    "https://officialchansneakers.com/image/cache/catalog/d_social_login/27-570x570.jpeg]"]
-
     ],
-    ["Bottega Vaenta Clutch 4","$289.00","Categories Bags Bottega Veneta" ,`Description
+    ["Bottega Vaenta Clutch 1","$289.00","Categories Bags Bottega Veneta" ,`Description
 
     Product code #191213-23
     100% Genuine Leather Matching Quality of Original Louis Vuitton Production (imported from Europe)
@@ -65,36 +50,70 @@ let demo = [
     Our Guarantee: The handbag you receive will look exactly as pictured on our professionally shot photos on our website (of our own stock) in terms of quality and description! Order from Designer Discreet and experience the difference today!
     
     Receive 15% off when you pay through Moneygram, Western Union, or wire transfer.`,
-
-    ["https://officialchansneakers.com/image/cache/catalog/d_social_login/48-570x570.jpeg",
-    "https://officialchansneakers.com/image/cache/catalog/d_social_login/44-570x570.jpeg",
-    "https://officialchansneakers.com/image/cache/catalog/Adidas/16-570x570.JPG",
-    "https://officialchansneakers.com/image/cache/catalog/d_social_login/27-570x570.jpeg]"]
 
     ]
     
 ]
 
+
+let demo2 = [
+    ["https://officialchansneakers.com/image/cache/catalog/d_social_login/48-570x570.jpeg",
+    "https://officialchansneakers.com/image/cache/catalog/d_social_login/48-570x570.jpeg"],
+    
+    ["https://officialchansneakers.com/image/cache/catalog/d_social_login/48-570x570.jpeg",
+    "https://officialchansneakers.com/image/cache/catalog/d_social_login/48-570x570.jpeg"],
+
+    ["https://officialchansneakers.com/image/cache/catalog/d_social_login/48-570x570.jpeg",
+    "https://officialchansneakers.com/image/cache/catalog/d_social_login/48-570x570.jpeg"],
+
+    ["https://officialchansneakers.com/image/cache/catalog/d_social_login/48-570x570.jpeg",
+    "https://officialchansneakers.com/image/cache/catalog/d_social_login/48-570x570.jpeg"]
+
+]
+
+
 const viewScrapedData = () => {
 
-    console.log(demo);
-    console.log(`==> ye rha <==`);
-    console.log(userProductFields);
+    //console.log(demo);
+    //console.log(`==> ye rha <==`);
     
+    let details = [];
 
     for(let i=0 ; i<demo.length ; i++){
-        
-        let fields_len = demo[i].length;
 
-        for(let j=0 ; j<fields_len ; j++ ){
-            
-            if(Array.isArray(demo[i][j])){
-                //console.log(j);
-            }
+        let temp = [];
+        let temp2 = [];
 
+        //for details
+        for(let j=0 ; j<demo[i].length ; j++){
+            temp.push(demo[i][j]);
         }
 
+        for(let k=0 ; k<demo2[i].length ; k++){
+            temp2.push(demo2[i][k]);            
+        }
+        
+        details.push([...temp,[...temp2]]);
+
     }
+   
+    
+    let table_elem = document.getElementById("scraped-product-table");
+
+    let heading_html = `<thead><tr>`
+
+    let headings_data = [];
+
+    for(let i=0 ; i<userProductFields.length ; i++){
+        heading_html += `<th> ${userProductFields[i]} </th>`
+    }
+
+    heading_html += `</tr></thead>`
+    
+    table_elem.innerHTML = heading_html;
+    console.log(heading_html);
+
+
 
 
 }
