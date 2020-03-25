4654e7d6c4d8326c5bf98f256ed1852c18c11d9b
h26k2 <hasnain.karim262@outlook.com>
2020-03-25 17:11:09 +0500

    some updates

---
+++ public/scripts/actions.js
@@ -0,0 +1,32 @@
+
+//Main Action
+
+const performAction = (action) => {
+
+
+    actionCol = document.getElementById("action-perform-col");
+
+    let action_col = actionCol.options[actionCol.options.selectedIndex].value;
+
+    if(action == "search"){
+
+    }
+    else if(action == "replace"){
+
+    }
+    else if(action == "append"){
+        
+    }
+    else if(action == "prepend"){
+        
+    }
+    else if(action == "maths"){
+        
+    }
+
+}
+
+const searchAction = (colName) => {
+
+}
+
