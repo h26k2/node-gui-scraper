a5a7e9d459f88906ecd33cfe17807d905cd78aaa
h26k2 <hasnain.karim262@outlook.com>
2020-03-25 18:15:03 +0500

    some updates

--- public/scripts/actions.js
+++ public/scripts/actions.js
@@ -5,14 +5,36 @@ const performAction = (action) => {
 
 
     actionCol = document.getElementById("action-perform-col");
+    let tableElem = document.getElementById("scraped-product-table");
 
     let action_col = actionCol.options[actionCol.options.selectedIndex].value;
-
-    if(action == "search"){
-
+    let inputtedData = document.getElementById("user-input-action").value;
+    
+    if(inputtedData.length < 1){
+        alert(`Please Enter data to perform action...`);
+        return;
     }
-    else if(action == "replace"){
 
+
+    //Finding the requrired index of column of the table
+
+    let headings = tableElem.getElementsByTagName("th");
+    let requrired_index = undefined;
+
+    for(let i=0 ; i<headings.length ;i++){
+        if(action_col == headings[i].innerText.toLowerCase()){
+            requrired_index = i;
+        }
+    }
+
+    if(requrired_index == undefined){
+        alert(`Unable to find colum, to perform action...`);
+        return;
+    }
+
+
+    if(action == "replace"){
+        let search = prompt("Input data you want to replace with ? ");
     }
     else if(action == "append"){
         
@@ -26,7 +48,7 @@ const performAction = (action) => {
 
 }
 
-const searchAction = (colName) => {
+const replaceAction = () => {
 
 }
 
