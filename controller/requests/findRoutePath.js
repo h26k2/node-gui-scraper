2ea50df13c429d01bbc5625438b0a60529cc8473
h26k2 <hasnain.karim262@outlook.com>
2020-04-02 11:40:31 +0500

    improved query recognizition

--- controller/requests/findRoutePath.js
+++ controller/requests/findRoutePath.js
@@ -18,40 +18,46 @@ const findRoutePath = (baseURL,productPath,page) => {
         else if(type == "query"){
             
             if(newURL.match(/\?/)){
-               
-                let symbol_index = newURL.indexOf(`${symbol}`);
-                symbol_index += symbol.length;
-               
-                if(newURL[symbol_index] == "="){
-                    symbol_index++;
-                    i = symbol_index;
-                    //finding the number which the current parameter has
-                    let number = "";
+                
+                let onlyQueries = newURL.substr(newURL.indexOf("?"),newURL.length - 1);
+                
+                if(onlyQueries.match(`${symbol}`)){
                     
-                    if(i == newURL.length - 1){
-                        number = newURL[i];
-                        newURL = newURL.replace(`${symbol}=${number}`,`${symbol}=${page}`);
-                    }
-                    else{
-                        while(i <= newURL.length - 1){
-                            if(newURL[i] != "&"){
-                                number += newURL[i];
-                                i++;
-                            }
-                            else{
-                                break;
-                            }
+                    let symbol_index = onlyQueries.indexOf(`${symbol}`);
+                    symbol_index += symbol.length;
+                
+                    if(onlyQueries[symbol_index] == "="){
+                        symbol_index++;
+                        i = symbol_index;
+                        //finding the number which the current parameter has
+                        let number = "";
+                        
+                        if(i == onlyQueries.length - 1){
+                            number = onlyQueries[i];
+                            onlyQueries = onlyQueries.replace(`${symbol}=${number}`,`${symbol}=${page}`);
                         }
+                        else{
+                            while(i <= onlyQueries.length - 1){
+                                if(onlyQueries[i] != "&"){
+                                    number += onlyQueries[i];
+                                    i++;
+                                }
+                                else{
+                                    break;
+                                }
+                            }
+                            onlyQueries = onlyQueries.replace(`${symbol}=${number}`,`${symbol}=${page}`);
+                        }
+                        let onlyURL = newURL.substr(0,newURL.indexOf("?"));
+                        newURL = onlyURL + onlyQueries;
                     }
-                    
-                    newURL = newURL.replace(`${symbol}=${number}`,`${symbol}=${page}`);
                     
                 }
-
-               
-
+                else{
+                    newURL += `&${symbol}=${page}`
+                }
             }
-            else{
+            else{console.log(`else wala`);
                 newURL += `?${symbol}=${page}`
             }
 
@@ -64,7 +70,8 @@ const findRoutePath = (baseURL,productPath,page) => {
         newURL = val.replace(/\{[0-9]{1,4}\}/gi,page)
        
     }
-
+    console.log(newURL , `final`);
+    
     //return newURL;
 
 }
